69d6086b353b202f59c691fe3ee9e10364cd7f9c
Zac Livingston <zacl@codeaurora.org>
2013-10-23 18:14:17 -0600

    Assign Qualcomm Atheros Copyright to Linux Foundation
    Change-Id: Ia774d13be1d0468df4a8ebbb1766070f0744e46a
    Signed-off-by: Zac Livingston <zacl@codeaurora.org>

--- hyfi-netfilter/hyfi_api.h
+++ hyfi-netfilter/hyfi_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Qualcomm Atheros, Inc.
+ * Copyright (c) 2011, The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
--- hyfi-netfilter/hyfi_netlink.c
+++ hyfi-netfilter/hyfi_netlink.c
@@ -2,7 +2,7 @@
  *  Hy-Fi Netlink
  *  QCA HyFi Netfilter
  *
- * Copyright (c) 2012 Qualcomm Atheros, Inc.
+ * Copyright (c) 2012 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
