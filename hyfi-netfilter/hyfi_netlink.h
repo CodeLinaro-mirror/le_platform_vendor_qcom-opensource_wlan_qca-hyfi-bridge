9ce68d026618d2adbd020f512e77c37e3fa82b2d
Sandeep Tomer <quic_stomer@quicinc.com>
2023-03-23 15:59:37 +0530

    [qca-hyfi-bridge] Add Compatibility number to 1 in Ezmesh Version Info.
    Change-Id: I9f434b740f11e5ce222f6b341162f863bae635f3
    Signed-off-by: Sandeep Tomer <quic_stomer@quicinc.com>

--- hyfi-netfilter/hyfi_netlink.h
+++ hyfi-netfilter/hyfi_netlink.h
@@ -53,7 +53,7 @@ void hyfi_netlink_fini(void);
 #define MAP_PKG_NAME "easymesh"
 #define MAP_MAJOR_VERION    12
 #define MAP_MINOR_VERION    0
-#define MAP_COMPATIBILITY   0
+#define MAP_COMPATIBILITY   1
 #define MAP_SU_BUILD        0
 
 /* character length assumption :
